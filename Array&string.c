610e763b7de524fab5643c14f9593ff3a51549dc
Amit meena <mea55629@gmail.com>
2024-08-24 22:28:02 +0530

    Add files via upload

---
+++ Array&string.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+
+
+
+int main() {
+    // int myarrays[]={10,20,30,40,50,60,70};
+    //   int i;
+    // int length=sizeof(myarrays) / sizeof(myarrays[0]);
+    // char firststring[]="Hello_World!";
+    // char secondstring[]="Hello_Raj";
+    //  char thirdstring[]="Deepak_Kushwah";
+    // char fourstring[]="riya";
+    // fourstring[0]='j';
+    //  char carName[] = "Volvo";
+    // printf("%d",myarrays[0]);
+    
+    // myarrays[2]=23;
+    
+    //  printf("%d",myarrays[2]);
+    
+    // for(int i=0;i<7;i++){
+    //   printf("%d\n",myarrays[i]);
+    // }
+    
+    //  printf("%lu",sizeof(myarrays));
+    
+    // printf("%d\n",length);
+    
+    // for(int i=0;i<length;i++){
+    //   printf("%d\n",myarrays[i]);
+    //  }
+    
+    //  printf("%s\n",firststring);
+    //  printf("%s\n",secondstring);
+    //  printf("%s\n",thirdstring);
+
+    // printf("%c\n",thirdstring[0]);
+    
+    //  printf("%s\n",fourstring);
+    
+    
+    //  for(int i=0;i<5;i++){
+    //   printf("%c\n",carName[i]);
+    //   }
+
+
+    return 0;
+}
