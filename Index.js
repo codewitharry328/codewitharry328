d0464ac7d3185d438fdb10298c230fa7b5e3144a
Amit meena <mea55629@gmail.com>
2024-09-21 10:50:19 +0530

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,176 @@
+// fullname = "amit meena";
+// age = 24;
+// price = 22.23;
+// x = null;
+// y = undefined;
+// isfollow = true;
+
+// fullname = 25
+// console.log(fullname);
+
+// let fullname = "Amit";
+
+// console.log(fullname);
+
+// const pi = 2.23;
+
+// console.log(pi);
+
+// let a;
+// a = 20;
+// console.log(a);
+
+// const student = {
+//     fullName: "Amit meena",
+//     age: 20,
+//     cgpa: 8.2,
+//     ispaas: true,
+// };
+
+// student["name"] = "Amit singh"
+
+// console.log(student["name"]);
+
+// const profile = {
+//     username: "@amit",
+//     isfollow: false,
+//     followers: 123,
+//     following: 123,
+// };
+
+// console.log(typeof profile["followers"]);
+
+// Arithmetic Operators
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a,"&b =", b);
+// console.log("a + b =", a + b);
+// console.log("a * b =", a * b);
+// console.log("a / b =", a / b);
+// console.log("a % b =", a % b);
+// console.log("a ** b =", a ** b);
+
+// unary operetor
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a,"&b =", b);
+// // a = a - 1;
+// console.log("a-- = ", a--);
+// console.log("a = ", a);
+
+// Assignment operator
+
+// let a = 5;
+// let b = 2;
+
+// a **= 4; // a = a * 4 +, -,*, /, %, **
+// console.log("a = ", a);
+
+// comparision operators
+
+// let a = 5; //number
+// let b = "5"; //string -> number
+
+// console.log("5 == 5", a == b); //true ya false
+// console.log("5 != 5", a != b);
+// console.log("a == b", a == b);
+// console.log("a === b", a === b);
+// console.log("a !== b", a !== b);
+
+// let a = 6; 
+// let b = 5;
+
+// console.log("5 < 3", a < b);
+// console.log("5 <= 5", a <= b);
+// console.log("6 >= 5", a >= b);
+
+//  let a = 6; 
+//  let b = 5;
+
+//  let cond1 = a < b; //false
+//  let cond2= a === 6; //true
+//  console.log("cond1 || cond2 = ", cond1 || cond2); //&&, ||, !
+//  console.log("!(6<5) = ", !(a < b));
+//  console.log("!(6<5) = ", !(a === 6));
+
+// conditional statement
+
+// let age = 14;
+// let mode = "light";
+// let color;
+
+// if(mode === "dark") {
+//     color = "black";
+// } else {
+//     color = "white";
+// }
+
+// if(mode === "light") {
+//     color = "white";
+// }
+
+
+// console.log(color);
+
+// if(age >= 18) {
+//     console.log("you can vot");
+// }
+
+// if(age < 18) {
+//     console.log("you CANNOT vot");
+// }  pahle ye
+
+// let age = 14;
+
+// if(age >= 18) {
+//     console.log("vote");
+// } else {
+//     console.log(" not vote");
+// }
+
+// even or odd
+
+// let num = 14;
+
+// if (num % 2 === 0) {
+//     console.log(num, "is even");
+// }else {
+//     console.log(num, "is odd");
+// }
+
+// else if
+
+// let mode = "silver";
+// let color;
+
+// if (mode === "dark") {
+//     color = "black"
+// }else if (mode === "blue") {
+//     color = "blue"
+// }else if (mode === "pink") {
+//     color = "pink"
+// }else {
+//     color = "white";
+// }
+
+// console.log(color);
+
+// Ternary operators
+
+// let age = 25;
+
+// let result = age >= 18 ? "adult" : "not adult";
+// console.log(result); ||ya
+// age >= 18 ? console.log("adult") : console.log("not adult"); //simple, compact if else
+
+let num = prompt("enter a number:");
+
+if (num % 5 === 0) {
+    console.log(num," is multiple of 5");
+} else {
+    console.log(num," is NOT a multiple of 5");
+}
