27f1813c142c00c34799416abf91083b5841c033
Amit meena <mea55629@gmail.com>
2024-11-09 01:41:54 -0500

    Add files via upload

---
+++ BeautifulGirl.js
@@ -0,0 +1,63 @@
+// practice Qs2
+
+// let score = prompt("enter your score (0-100):"); //promte se
+// let grade;
+
+// if (score >= 90 && score <= 100) {
+//     grade = 'A';
+// } else if (score >= 70 && score <= 89) {
+//     grade = 'B';
+// } else if (score >= 60 && score <= 69) {
+//     grade = 'C';
+// } else if (score >= 50 && score <= 59) {
+//     grade = 'D';
+// } else if (score >= 0 && score <= 49) {
+//     grade = 'f';
+// }
+
+// console.log("according to your score, your grade was : ", grade);
+
+// loop & string
+
+// for (let count = 1; count <= 5; count++) {
+//     console.log("amit meena");
+// }
+
+// let sum = 0;
+// let n = 6
+// for(let i = 1; i <= n; i++) {
+//     sum = sum + i;
+// }
+// console.log("sum = ", sum);
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log("i = ", i);
+// }
+
+//  while loop
+
+// let i = 1;
+// while(i <= 10) {
+//     console.log("amit meena");
+//     i++;
+// }
+
+// do-while loop
+
+// let i = 20;
+// do {
+//     console.log("amit");
+//     i++;
+// } while (i <= 10);
+
+// for of loop
+
+// let str = "JavaScript";
+// let size = 0;
+
+// for(let val of str) { // iterator -> charactor
+//     console.log("val=", val);
+//     size++;
+// }
+
+// console.log("string size = ", size);
