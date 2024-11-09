2a71adfd45eef9ae405a8f455b8b3e13907831d8
Amit meena <mea55629@gmail.com>
2024-11-09 01:42:52 -0500

    Add files via upload

---
+++ boobs.js
@@ -0,0 +1,103 @@
+// variable
+
+// var number1 = 21;
+// var number2 = 21;
+// console.log(number1 + number2);
+
+// strings
+
+// let str = "amitmeena";
+// console.log(str[1]);
+
+// JavaScript String Methods
+
+// let str = "amitmeena";
+// let newstr = str.toUpperCase();
+// console.log(str);
+// console.log(newstr);
+
+// let str = "amitmeena";
+// str = str.toLowerCase();
+// console.log(str);
+
+// let str = "     Amit    meena    developer";
+// console.log(str.trim());
+
+// let str = "meena";
+// console.log(str.slice(3, 5));
+
+// let str1 = "amit";
+// let str2 = "meena";
+// // let str= str2.concat(str1);
+// let res = "I am learning coding from my slef" + str1 + str2;
+// console.log(res);
+
+// str replace
+
+// let str = "hellololo";
+// console.log(str.replaceAll("lo", "p"));
+
+// let str = "ILoveJS";
+// str = str.replace("I", "S");
+// console.log(str);
+// console.log(str.charAt(4));
+
+// Array in js
+
+// let marks = [94, 82, 75, 64, 36];
+// console.log(marks);
+// console.log(marks.length);
+
+// let heroes = ["ironman", "thor", "hulk", "spiderman"];
+// console.log(heroes);
+
+// let heroes = ["ironman", "thor", "hulk", "spiderman"];
+// for(let idx = 0; idx < heroes.length; idx++){  //i ko iterable bi kahte hai
+//     console.log(heroes[idx]);
+// }
+
+// for (let hero of heroes) {
+//     console.log(hero);
+// }
+
+// let cities = ["delhi", "pune", "mumbai", "hydrabad", "kotputli"];
+// for (let city of cities) {
+//     console.log(city.toUpperCase());
+// }
+
+// array concat
+
+// let marvalHeroes = ["ironman", "spiderman", "thor"];
+// let dcheroes = ["superman", "batman"];
+// let indianHeroes = ["krish", "flyjatt", "shakiman"];
+
+// let heroes = marvalHeroes.concat(dcheroes, indianHeroes);
+// console.log(heroes);
+
+// let marvalHeroes = ["ironman", "spiderman", "thor"];
+// let val = marvalHeroes.shift();
+// console.log("deleted", val); 
+
+// function in js
+
+// function myFunction() {
+//     console.log("welcome to amit meena");
+//     console.log("we are learning js");
+// }
+// myFunction();
+
+// function myFunction(msg, n) {
+//     console.log(msg * n);
+// }
+// myFunction("I love sexy girl", 100);
+
+// function -> 2 number
+
+// function sum(x, y){
+//     s = x + y;
+//     console.log("before return");
+//     return s;
+//     console.log("after return");
+// }
+// let val = sum(3, 4);
+// console.log(val);
